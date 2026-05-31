5c9910583114de97bf2eccc5255d4121f79b3a7d
Trey Dedman <treydedman@gmail.com>
2026-05-31 14:46:25 -0500

    feat: add favicon, og image, and metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,17 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "CyBear Lock",
   description: "The Secure Way to Forget Your Passwords",
+  openGraph: {
+    title: "CyBear Lock",
+    description:
+      "A fast, simple, and secure password manager designed to take the hassle out of remembering your login details.",
+    images: ["/cybear-og.png"],
+    url: "https://cybear-lock-v2.vercel.app",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
